f23e85dca7f2b3799579dc37bc7388695e5f2416
DavidGoegelein <davidgoegelein@gmail.com>
2021-11-24 18:12:59 +0100

    Updated Menu, New Activities Implemented: EditDiagramView ToDoList BudgetLimit

--- app/src/main/java/com/example/haushaltsplaner/BudgetLimit.java
+++ app/src/main/java/com/example/haushaltsplaner/BudgetLimit.java
@@ -5,61 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public class ShowOutgosActivity extends AppCompatActivity {
+public class BudgetLimit extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_outgos);
-
-        TextView tv = (TextView) findViewById(R.id.textViewOutgoes);
-
-        Intent intent = getIntent();
-        List<Outgo> list = (List<Outgo>) intent.getSerializableExtra("list");
-        String text = "Alle Ausgaben des aktuellen Monats:";
-
-        for (int i = 0; i < list.size(); i++) {
-            text = text + " '\n' " + list.get(i).toString();
-        }
-        tv.setText(text);
-
+        setContentView(R.layout.activity_budget_limit);
     }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.showoutgoes_menu, menu);
+        inflater.inflate(R.menu.budgetlimit_menu, menu);
         return true;
     }
 
-    public void changeEntry(View view) {
-        EditText id = (EditText) findViewById(R.id.textViewEditText);
-
-        int valueId = -1;
-        if (!TextUtils.isEmpty(id.getText())) {
-            valueId = Integer.parseInt(id.getText().toString());
-        }
-        Intent intent = new Intent();
-        intent.putExtra("entry", "outgo");
-        intent.putExtra("id", valueId);
-        setResult(RESULT_OK, intent);
-        super.finish();
-    }
-
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
+        switch (item.getItemId()){
+
 
             case R.id.itemStartseite:
                 Intent switchToMain = new Intent(this, MainActivity.class);
@@ -69,11 +38,6 @@ public class ShowOutgosActivity extends AppCompatActivity {
             case R.id.itemEinnahmenAusgaben:
                 Intent switchToAddEntry = new Intent(this, AddEntryActivity.class);
                 startActivity(switchToAddEntry);
-                return true;
-
-            case R.id.itemBudgetLimit:
-                Intent switchToBudgetLimit = new Intent(this, BudgetLimit.class);
-                startActivity(switchToBudgetLimit);
                 return true;
 
             case R.id.itemDiagrammansicht:
@@ -90,6 +54,7 @@ public class ShowOutgosActivity extends AppCompatActivity {
                 Intent switchToDoList = new Intent(this, ToDoList.class);
                 startActivity(switchToDoList);
                 return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
--- app/src/main/java/com/example/haushaltsplaner/EditDiagramView.java
+++ app/src/main/java/com/example/haushaltsplaner/EditDiagramView.java
@@ -5,61 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.LinkedList;
-import java.util.List;
-
-public class ShowOutgosActivity extends AppCompatActivity {
+public class EditDiagramView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_outgos);
-
-        TextView tv = (TextView) findViewById(R.id.textViewOutgoes);
-
-        Intent intent = getIntent();
-        List<Outgo> list = (List<Outgo>) intent.getSerializableExtra("list");
-        String text = "Alle Ausgaben des aktuellen Monats:";
-
-        for (int i = 0; i < list.size(); i++) {
-            text = text + " '\n' " + list.get(i).toString();
-        }
-        tv.setText(text);
-
+        setContentView(R.layout.activity_edit_diagram_view);
     }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.showoutgoes_menu, menu);
+        inflater.inflate(R.menu.editdiagramview_menu, menu);
         return true;
     }
 
-    public void changeEntry(View view) {
-        EditText id = (EditText) findViewById(R.id.textViewEditText);
-
-        int valueId = -1;
-        if (!TextUtils.isEmpty(id.getText())) {
-            valueId = Integer.parseInt(id.getText().toString());
-        }
-        Intent intent = new Intent();
-        intent.putExtra("entry", "outgo");
-        intent.putExtra("id", valueId);
-        setResult(RESULT_OK, intent);
-        super.finish();
-    }
-
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
+        switch (item.getItemId()){
 
             case R.id.itemStartseite:
                 Intent switchToMain = new Intent(this, MainActivity.class);
@@ -74,11 +39,6 @@ public class ShowOutgosActivity extends AppCompatActivity {
             case R.id.itemBudgetLimit:
                 Intent switchToBudgetLimit = new Intent(this, BudgetLimit.class);
                 startActivity(switchToBudgetLimit);
-                return true;
-
-            case R.id.itemDiagrammansicht:
-                Intent switchToEditDiagramView = new Intent(this, EditDiagramView.class);
-                startActivity(switchToEditDiagramView);
                 return true;
 
             case R.id.itemKalender:
