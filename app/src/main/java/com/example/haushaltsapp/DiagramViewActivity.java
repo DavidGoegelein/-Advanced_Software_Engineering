bf5fbcbd1dd05ebcd7fb309abd27f4a547e7d455
DavidGoegelein <davidgoegelein@gmail.com>
2021-12-04 14:02:01 +0100

    Hinzufügen von To Do Listen, Zusammenfügen der Datenbank, Anlegen von Annual View

---
+++ app/src/main/java/com/example/haushaltsapp/DiagramViewActivity.java
@@ -0,0 +1,80 @@
+package com.example.haushaltsapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class DiagramViewActivity extends AppCompatActivity {
+
+    private Button changeToAnnual;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diagram_view);
+        changeToAnnual = findViewById(R.id.changeView);
+    }
+
+
+    public void changeToAnnual(View view) {
+        Intent switchToAnnualView= new Intent(this, AnnualViewActivity.class);
+        startActivity(switchToAnnualView);
+        }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.diagramview_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.itemStartseite:
+                Intent switchToMain = new Intent(this, MainActivity.class);
+                startActivity(switchToMain);
+                return true;
+
+            case R.id.itemEinnahmenAusgaben:
+                Intent switchToAddEntry = new Intent(this, AddEntryActivity.class);
+                startActivity(switchToAddEntry);
+                return true;
+
+            case R.id.itemBudgetLimit:
+                Intent switchToBudgetLimit = new Intent(this, BudgetLimitActivity.class);
+                startActivity(switchToBudgetLimit);
+                return true;
+
+            case R.id.itemTabelle:
+                Intent switchToChart = new Intent(this, ChartViewActivity.class);
+                startActivity(switchToChart);
+                return true;
+
+            case R.id.itemKalender:
+                Intent switchToCalendar = new Intent(this, CalendarEventActivity.class);
+                startActivity(switchToCalendar);
+                return true;
+
+            case R.id.itemTodoListe:
+                Intent switchToToDoList = new Intent(this, ToDoListActivity.class);
+                startActivity(switchToToDoList);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
